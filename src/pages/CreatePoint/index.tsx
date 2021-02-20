38e3c0f23e0916a44a563be7be4066acccb4bfda
augustolimads <augustolimads@gmail.com>
2021-02-20 16:16:58 -0300

    ends registration

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,19 @@
-import React, { useState, useEffect, ChangeEvent, useRef, useMemo, useCallback } from "react";
+import React, {
+	useState,
+	useEffect,
+	ChangeEvent,
+	FormEvent,
+	useRef,
+	useMemo,
+	useCallback,
+} from "react";
 import "./styles.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import logo from "../../assets/logo.svg";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import api from "../../services/api";
 import axios from "axios";
-import { LeafletMouseEvent } from "leaflet";
 
 interface Item {
 	id: number;
@@ -27,28 +34,34 @@ interface IBGECityResponse {
 }
 
 const CreatePoint = () => {
-	const center:[number,number] = [-8.2648232, -35.9896734];
+	const history = useHistory();
+	const center: [number, number] = [-8.2648232, -35.9896734];
 	const [items, setItems] = useState<Item[]>([]);
 	const [ufs, setUfs] = useState<string[]>([]);
 	const [cities, setCities] = useState<string[]>([]);
 	const [selectedUf, setSelectedUf] = useState("0");
 	const [selectedCity, setSelectedCity] = useState("0");
-	const [initialPosition, setInitialPosition] = useState<[number, number]>(center);
-	const [selectedPosition, setSelectedPosition] = useState<[number, number]>(center);
-
+	const [selectedItems, setSelectedItems] = useState<number[]>([]);
+	const [initialPosition, setInitialPosition] = useState<[number, number]>(
+		center
+	);
+	const [selectedPosition, setSelectedPosition] = useState<[number, number]>(
+		center
+	);
 	const [draggable, setDraggable] = useState(true);
 	const markerRef = useRef<any>(null);
-	const eventHandlers = useMemo(
-		() => ({
-			dragend() {
-				const marker = markerRef.current;
-				if (marker != null) {
-					setSelectedPosition(marker.getLatLng());
-				}
-			},
-		}),
-		[]
-	);
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		whatsapp: "",
+	});
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const { latitude, longitude } = position.coords;
+			setInitialPosition([latitude, longitude]);
+		});
+	}, []);
 
 	useEffect(() => {
 		api.get("items").then((response) => {
@@ -79,9 +92,20 @@ const CreatePoint = () => {
 			.then((response) => {
 				const cityNames = response.data.map((city) => city.nome);
 				setCities(cityNames);
-				console.log(response.data);
 			});
 	}, [selectedUf]);
+
+	const eventHandlers = useMemo(
+		() => ({
+			dragend() {
+				const marker = markerRef.current;
+				if (marker != null) {
+					setSelectedPosition(marker.getLatLng());
+				}
+			},
+		}),
+		[]
+	);
 
 	function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
 		const uf = event.target.value;
@@ -91,6 +115,47 @@ const CreatePoint = () => {
 	function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
 		const city = event.target.value;
 		setSelectedCity(city);
+	}
+
+	function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+		const { name, value } = event.target;
+		setFormData({ ...formData, [name]: value });
+	}
+
+	function handleSelectItem(id: number) {
+		const alreadySelected = selectedItems.findIndex((item) => item === id);
+		if (alreadySelected >= 0) {
+			const filteredItems = selectedItems.filter((item) => item !== id);
+			setSelectedItems(filteredItems);
+		} else {
+			setSelectedItems([...selectedItems, id]);
+		}
+	}
+
+	async function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+		const { name, email, whatsapp } = formData;
+		const uf = selectedUf;
+		const city = selectedUf;
+		const [latitude, longitude] = selectedPosition;
+		const items = selectedItems;
+
+		console.log(latitude, longitude)
+
+		const data = {
+			name,
+			email,
+			whatsapp,
+			uf,
+			city,
+			latitude,
+			longitude,
+			items,
+		};
+
+		await api.post('points', data)
+		alert('ponto de coleta criado!')
+		history.push('/');
 	}
 
 	return (
@@ -104,7 +169,7 @@ const CreatePoint = () => {
 					</Link>
 				</header>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<h1>Cadastro do ponto de coleta</h1>
 
 					<fieldset>
@@ -114,13 +179,23 @@ const CreatePoint = () => {
 
 						<div className="field">
 							<label htmlFor="name">Nome da entidade</label>
-							<input type="text" name="name" id="name" />
+							<input
+								type="text"
+								name="name"
+								id="name"
+								onChange={handleInputChange}
+							/>
 						</div>
 
 						<div className="field-group">
 							<div className="field">
 								<label htmlFor="name">E-mail</label>
-								<input type="email" name="email" id="email" />
+								<input
+									type="email"
+									name="email"
+									id="email"
+									onChange={handleInputChange}
+								/>
 							</div>
 							<div className="field">
 								<label htmlFor="name">WhatsApp</label>
@@ -128,6 +203,7 @@ const CreatePoint = () => {
 									type="text"
 									name="whatsapp"
 									id="whatsapp"
+									onChange={handleInputChange}
 								/>
 							</div>
 						</div>
@@ -139,10 +215,7 @@ const CreatePoint = () => {
 							<span>Selecione o endereço no mapa</span>
 						</legend>
 
-						<MapContainer
-							center={selectedPosition}
-							zoom={15}
-						>
+						<MapContainer center={initialPosition} zoom={15}>
 							<TileLayer
 								attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 								url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -153,8 +226,7 @@ const CreatePoint = () => {
 								eventHandlers={eventHandlers}
 								position={selectedPosition}
 								ref={markerRef}
-							>
-							</Marker>
+							></Marker>
 						</MapContainer>
 
 						<div className="field-group">
@@ -204,7 +276,17 @@ const CreatePoint = () => {
 						<ul className="items-grid">
 							{items.map((item) => {
 								return (
-									<li key={item.id}>
+									<li
+										key={item.id}
+										onClick={() =>
+											handleSelectItem(item.id)
+										}
+										className={
+											selectedItems.includes(item.id)
+												? "selected"
+												: ""
+										}
+									>
 										<img
 											src={item.image_url}
 											alt={item.title}
